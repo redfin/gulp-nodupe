b90c6ec467e2be64e8db84faf645f7675be611bc
Doug Wade <doug.wade@redfin.com>
2016-01-06 17:14:27 -0800

    respond to pr feedback

--- index.babel.js
+++ index.babel.js
@@ -1,25 +1,29 @@
 let gutil = require('gulp-util');
 let PluginError = gutil.PluginError;
 let through = require('through2');
+let crypto = require('crypto');
 
 const PLUGIN_NAME = 'gulp-break-on-duplicate-svg';
 
 let svgs = new Set();
 
-function isDuplicate(file) {
+function isDuplicate(hash) {
 
-	if (svgs.has(file.contents.toString('utf8'))) {
+	if (svgs.has(hash)) {
 		return true;
 	}
 
-	svgs.add(file.contents.toString('utf8'));
+	svgs.add(hash);
 
 	return false;
 }
 
 function breakOnDuplicateSvg() {
 	return through.obj((file, encoding, callback) => {
-		if (isDuplicate(file)) {
+		let md5 = crypto.createHash('md5');
+		md5.update(file.contents.toString('utf8'))
+		var hash = md5.digest('hex');
+		if (isDuplicate(hash)) {
 			callback(new PluginError(PLUGIN_NAME, "SVG Store contains duplicate element: " + file.path), null);
 		} else {
 			callback(null, file);
