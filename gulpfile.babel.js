58a5cd7b295b3d7f21436381c441ad811b78a24c
doug.wade <doug.wade@redfin.com>
2016-02-24 17:22:47 -0800

    add linting target to compile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 let babel = require('gulp-babel');
 let del = require('del');
+let eslint = require('gulp-eslint');
 let gulp = require('gulp');
+let gutil = require('gulp-util');
 let nsp = require('gulp-nsp');
 let path = require('path');
 let paths = require('./paths');
@@ -10,6 +12,7 @@ gulp.task('nsp', (cb) => nsp({package: path.resolve('package.json')}, cb));
 
 gulp.task('compile', () => {
   gulp.src(paths.src)
+    .pipe(eslint())
     .pipe(babel())
     .pipe(rename('index.js'))
     .pipe(gulp.dest(paths.dest));
@@ -17,8 +20,8 @@ gulp.task('compile', () => {
 
 gulp.task('clean', () => {
   del([path.join(paths.dest, 'index.js')]).then(paths => {
-    console.log('Deleted files and folders:\n', paths.join('\n'))
-  })
+    gutil.log('Deleted files and folders:\n', paths.join('\n'));
+  });
 });
 
 gulp.task('prepublish', ['nsp', 'compile']);
--- index.babel.js
+++ index.babel.js
@@ -21,14 +21,14 @@ function isDuplicate(hash) {
 function nodupe() {
   return through.obj((file, encoding, callback) => {
     let md5 = crypto.createHash('md5');
-    md5.update(file.contents.toString('utf8'))
+    md5.update(file.contents.toString('utf8'));
     var hash = md5.digest('hex');
     if (isDuplicate(hash)) {
-      callback(new PluginError(PLUGIN_NAME, "Stream contains duplicate element: " + file.path), null);
+      callback(new PluginError(PLUGIN_NAME, 'Stream contains duplicate element: ' + file.path), null);
     } else {
       callback(null, file);
     }
   });
-};
+}
 
 module.exports = nodupe;
